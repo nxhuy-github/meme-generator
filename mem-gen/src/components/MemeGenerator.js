94670bd41571c381d2674fb8892460af8f1e4abe
NGUYEN XUAN HUY p1419771 <23189942+nxhuy-github@users.noreply.github.com>
2019-09-12 16:13:20 +0200

    add elements to MemeGenerator Component

--- mem-gen/src/components/MemeGenerator.js
+++ mem-gen/src/components/MemeGenerator.js
@@ -1,9 +1,47 @@
 import React from 'react'
 
-function MemeGenerator() {
-  return (
-    <h1>MemeGenerator</h1>
-  )
+class MemeGenerator extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      topText: "",
+      botText: "",
+      randImg: "https://i.imgflip.com/1bij.jpg",
+      allMemeImgs: []
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    console.log("handle change")
+  }
+
+  handleSubmit(event) {
+    console.log("handle submit")
+  }
+  
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input 
+          type="text" 
+          name="topText" 
+          value={this.state.topText}
+          onChange={this.handleChange} 
+          placeholder="Top Text"/>
+        <input 
+          type="text" 
+          name="botText" 
+          value={this.state.botText}
+          onChange={this.handleChange} 
+          placeholder="Bottom Text"/>
+        <button>Gen</button>
+        <br/>
+        <img src={this.state.randImg} alt="Trolll"/>
+      </form>
+    )
+  }
 }
 
 export default MemeGenerator
