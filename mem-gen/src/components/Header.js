b278b56eac8f40b0a0410ea05c961d9f301b0a5d
NGUYEN XUAN HUY p1419771 <23189942+nxhuy-github@users.noreply.github.com>
2019-09-12 16:04:50 +0200

    first prototype of Header Component

--- mem-gen/src/components/Header.js
+++ mem-gen/src/components/Header.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Header() {
   return (
-    <h1>Header</h1>
+    <header>
+      <img src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface-PNG.png" alt="Trolllllll"/>
+      <p>Meme Generator</p>
+    </header>
   )
 }
 
