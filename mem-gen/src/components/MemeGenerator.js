e51f30c86ea996b6576b5f5a8af7a92bcaded5cf
NGUYEN XUAN HUY p1419771 <23189942+nxhuy-github@users.noreply.github.com>
2019-09-12 16:22:58 +0200

    the top&bottom text are working

--- mem-gen/src/components/MemeGenerator.js
+++ mem-gen/src/components/MemeGenerator.js
@@ -13,33 +13,52 @@ class MemeGenerator extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
+  componentDidMount() {
+    fetch("https://api.imgflip.com/get_memes")
+      .then(response => response.json())
+      .then(response => {
+        const {memes} = response.data
+        this.setState({
+          allMemeImgs: memes
+        })
+      })
+  }
+
   handleChange(event) {
-    console.log("handle change")
+    const {name, value} = event.target
+    this.setState({
+      [name]: value
+    })
   }
 
   handleSubmit(event) {
-    console.log("handle submit")
+    // refresh our page
   }
   
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input 
-          type="text" 
-          name="topText" 
-          value={this.state.topText}
-          onChange={this.handleChange} 
-          placeholder="Top Text"/>
-        <input 
-          type="text" 
-          name="botText" 
-          value={this.state.botText}
-          onChange={this.handleChange} 
-          placeholder="Bottom Text"/>
-        <button>Gen</button>
-        <br/>
-        <img src={this.state.randImg} alt="Trolll"/>
-      </form>
+      <div>
+        <form onSubmit={this.handleSubmit} className="meme-form">
+          <input 
+            type="text" 
+            name="topText" 
+            value={this.state.topText}
+            onChange={this.handleChange} 
+            placeholder="Top Text"/>
+          <input 
+            type="text" 
+            name="botText" 
+            value={this.state.botText}
+            onChange={this.handleChange} 
+            placeholder="Bottom Text"/>
+          <button>Gen</button>
+        </form>
+        <div className="meme">
+          <img src={this.state.randImg} alt="Trolll"/>
+          <h2 className="top">{this.state.topText}</h2>
+          <h2 className="bottom">{this.state.botText}</h2>
+        </div>
+      </div>
     )
   }
 }
